fc64630d7efde5a80b19b70b5bd16a135b17a091
tripnv <nandibirta@gmail.com>
2023-07-02 03:53:45 +0300

    removed leftover code

--- main.py
+++ main.py
@@ -2,57 +2,6 @@ import game
 import click
 
 
-# @click.group("run")
-# @click.option(
-#     "--headless/--render",
-#     default=True,
-#     help="Whether to display the game, or to run it headless",
-# )
-# def run(mode):
-#     agent = init_agent()
-#     environment = init_environment()
-#     environment.agent = agent
-
-#     if mode == "render":
-#         environment.run_sketch()
-#     else:
-#         click.echo(f"The {agent.search_algorithm} scored: {environment.headless_run()}")
-
-
-# @run.command()
-# @click.option(
-#     "-a",
-#     "--agent",
-#     default="human",
-#     type=click.Choice(["human", "bfs"]),
-#     help="Specify the agent type. Admissible values include 'human' and other search algorithms (e.g bfs)",
-# )
-# # @click.pass_context()
-# def init_agent(agent):
-#     if agent == "human":
-#         return None
-#     else:
-#         return game.Agent(agent)
-
-
-# @run.command()
-# @click.option(
-#     "-fr",
-#     "--frame-rate",
-#     default=60,
-#     type=int,
-#     help="Specify the frame rate of the displayed game. Defaults to 60",
-# )
-# def init_environment(fr):
-#     environment = game.Environment()
-#     environment.frame_rate = fr
-#     return environment
-
-
-# if __name__ == "__main__":
-#     run()
-
-
 @click.command()
 @click.option(
     "--agent",
